db640e99d0676b49eb0bc4f4e725027d925ad4c1
Connor Wood <connorwood71@gmail.com>
2026-02-27 19:22:52 +0000

    Add string_to_oct, which converts an octal string into a number

--- monitor.s
+++ monitor.s
@@ -13,41 +13,86 @@ start:
 	ELEF 2, banner
 	JSR print
 
-	ELEF 0, tst1
-	ELEF 1, tst2
-	PSH 0,1
-	JSR strcmp
-	POP 0,1  // Get the values back, although we don't really care about them much
-
-	MOV 2, 2, SNR
-	JMP start_tst1ne
-
-	ELEF 2, same_str
-	JSR print
-	JMP start_tst2
-
-start_tst1ne:
-	ELEF 2, diff_str
-	JSR print
-
-start_tst2:
 	ELEF 0, tst2
-	ELEF 1, tst3
-	PSH 0,1
-	JSR strcmp
-	POP 0,1  // Get the values back, although we don't really care about them much
+	PSH 0,0
+	JSR string_to_oct
+	MOV 1, 1, SZR
+	JMP start_right
 
-	MOV 2, 2, SNR
-	JMP start_tst2ne
-
-	ELEF 2, same_str
+	ELEF 2, wrong
 	JSR print
 	HALT
 
-start_tst2ne:
-	ELEF 2, diff_str
+start_right:
+	ELEF 2, right
 	JSR print
 	HALT
+
+	// string_to_oct - Turn a given string into an octal number
+	//
+	// Parameters:
+	// - Stack: string
+	//
+	// Returns:
+	// - AC1 = 0 (on success), 1 (on NAN)
+	// - AC2 = result
+	//
+	// Stack variables:
+	// - Offset 0: current string offset
+	// - Offset 1: current result
+string_to_oct:
+	SAVE 2
+
+	XOR 1, 1
+	STA 1, 1, 3
+
+	STA 1, 2, 3
+
+string_to_oct_loop:
+	LDA 2, -5, 3  // Load AC2 with the first string
+	ADD 1, 2
+
+	LDA 0, 0, 2   // Load the first character into the string
+	MOV 0, 0, SNR
+	JMP string_to_oct_done
+
+	ELEF 1, 060   // Character for zero
+	SGE 0, 1      // If the current character is less than '0'
+	JMP string_to_oct_nan
+
+	SUB 1, 0
+	ELEF 1, 7
+	SGE 1, 0      // If the result is greater than 7...
+	JMP string_to_oct_nan
+
+	// Result = (result << 3) + AC0
+	LDA 1, 2, 3
+	MOVZL 1, 1
+	MOVZL 1, 1
+	MOVZL 1, 1
+	ADD 0, 1
+	STA 1, 2, 3
+
+	// Next character
+	LDA 1, 1, 3
+	INC 1, 1
+	STA 1, 1, 3
+
+	JMP string_to_oct_loop
+
+string_to_oct_done:
+	LDA 1, 2, 3
+	STA 1, -2, 3
+	XOR 1, 1
+	STA 1, -3, 3
+	RTN
+
+string_to_oct_nan:
+	XOR 1, 1
+	INC 1, 1
+	STA 1, -3, 3
+	RTN
+	
 
 	// strcmp - Compare two strings
 	//
@@ -68,7 +113,7 @@ strcmp:
 	STA 1, 1, 3   // Place the current offset on the stack
 
 strcmp_loop:	
-	LDA 2, -7, 3  // Load AC2 with the first string
+	LDA 2, -5, 3  // Load AC2 with the first string
 	ADD 1, 2      // Add AC1 to AC2, and place in AC2
 
 	LDA 0, 0, 2   // Load the character from the first string in AC0
@@ -127,12 +172,11 @@ print_done:
 	RTN
 
 	var banner = "Eclipse FPS100 Resident Monitor v0.0.1\r\nAuthored by Venos\r\n\n"
-	var same_str = "Strings are the same!\r\n"
-	var diff_str = "Strings are different!\r\n"
+	var right = "Right\r\n"
+	var wrong = "Wrong\r\n"
 
-	var tst1 = "abc"
+	var tst1 = "123456"
 	var tst2 = "abc"
-	var tst3 = "def"
 
 	org 0600
 	HALT
