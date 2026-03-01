9776fe5e8755adba921c254f236b2bfeed44d641
Connor Wood <connorwood71@gmail.com>
2026-03-01 21:53:21 +0000

    Add a routinel to manage calling the various monitor commands.
    Add a deposit and an examine command. Currently, these just read and write
    to Eclipse memory, but that serves as a test. Hooking up the AP is next.

--- monitor.s
+++ monitor.s
@@ -29,30 +29,17 @@ doprompt:
 	ELEF 2, 8
 	PSH 0, 2
 	EJSR strtok
-	POP 0, 2
+	POP 2, 0
 
-	// Print out the first two tokens
-	ELEF 2, command_tokens
-	LDA 0, 0, 2
-	MOV 0, 0, SNR
-	JMP doprompt
+	ELEF 0, top_level_command_table
+	ELEF 1, command_str
+	ELEF 2, command_tokens + 1
+	ELEF 3, 7  // 8 possible locations, the first must be the command
+	PSH 0, 3
 
-	MOV 0, 2
-	EJSR print
+	EJSR cmdtbl
 
-	ELEF 2, nl
-	EJSR print
-
-	ELEF 2, command_tokens
-	LDA 0, 1, 2
-	MOV 0, 0, SNR
-	JMP doprompt
-
-	MOV 0, 2
-	EJSR print
-
-	ELEF 2, nl
-	EJSR print
+	POP 3, 0
 
 	JMP doprompt
 
@@ -62,10 +49,212 @@ doprompt:
 	var banner = "Eclipse FPS100 Resident Monitor v0.0.1\r\nAuthored by Venos\r\n\n"
 	var prompt = "> "
 	var nl = "\r\n"
+
+	// ============================================================
+	// Commands
+	// ============================================================
+	var deposit_command_name = "d"
+	var examine_command_name = "x"
+
+top_level_command_table:
+	var deposit_command_name_p = deposit_command_name
+	var deposit_command_func = dep
+	var examine_command_name_p = examine_command_name
+	var examine_command_func = exam
+	var end = 0
+	var end2 = 0
+
+	// dep - Deposit a value into the AP
+	//
+	// Parameters:
+	// - Stack: pointer to address string
+	// - Stack: pointer to value string
+	//
+	// Stack variables:
+	// - Offset 0: address to write to
+	//
+	// NOTE: for testing purposes, this doesn't (yet) talk to the AP
+dep:
+	SAVE 1
+
+	// First, convert the address into octal
+	LDA 2, -11, 3
+	MOV 2, 2, SNR
+	JMP dep_done
+
+	PSH 2, 2
+	EJSR string_to_oct
+	POP 0, 0
+
+	MOV 1, 1, SZR
+	JMP dep_done
+
+	// Save the result to the stack
+	STA 2, 1, 3
+
+	// Next, convert the value into octal
+	LDA 2, -10, 3
+	MOV 2, 2, SNR
+	JMP dep_done
+
+	PSH 2, 2
+	EJSR string_to_oct
+	POP 0, 0
+
+	MOV 1, 1, SZR
+	JMP dep_done
+
+	// Put value in AC1, address in AC2
+	MOV 2, 1
+	LDA 2, 1, 3
+
+	// Store the address
+	STA 1, 0, 2
+
+dep_done:
+	RTN
+
+	// exam - Examine a value from the AP
+	//
+	// Parameters:
+	// - Stack: pointer to address string
+	//
+	// NOTE: for testing purposes, this doesn't (yet) talk to the AP
+exam:
+	SAVE 1
+
+	// First, convert the address into octal
+	LDA 2, -11, 3
+	MOV 2, 2, SNR
+	JMP dep_done
+
+	PSH 2, 2
+	EJSR string_to_oct
+	POP 0, 0
+
+	MOV 1, 1, SZR
+	JMP exam_done
+
+	MOV 2, 0
+	ELEF 1, exam_result
+
+	PSH 0, 1
+	EJSR oct_to_string
+	POP 1, 0
+
+	ELEF 2, exam_result
+	EJSR print
+
+	ELEF 2, nl
+	EJSR print
+
+exam_done:
+	RTN
+
+	var exam_result resv 7
 	
 	// ============================================================
 	// Utility Functions
 	// ============================================================
+
+	// cmdtbl - Trigger a command from a command table
+	//
+	// Parameters:
+	// - Stack: the command table
+	// - Stack: the command string
+	// - Stack: pointer to the stackframe for the command to run
+	// - Stack: command stackframe size
+	//
+	// No return value
+	//
+	// Stack variables:
+	// - Offset 0: current command to test
+	//
+	// Command table structure:
+	// <pointer to NULL terminated command string> <pointer to routine to call>
+	// Last entry is defined by a pair of NULL pointers
+cmdtbl:
+	SAVE 1			// one local: current table entry pointer
+
+	// current = table pointer
+	LDA 0, -8, 3
+	STA 0, 1, 3
+
+cmdtbl_loop:
+	// AC2 = current entry pointer
+	LDA 2, 1, 3
+
+	// AC0 = table string pointer (*current)
+	LDA 0, 0, 2
+
+	// If NULL → end of table
+	MOV 0,0,SNR
+	JMP cmdtbl_done
+
+	// ---- strcmp(table_string, command_string) ----
+
+	LDA 1, -7, 3		// command string
+
+	PSH 0,1			// push table string
+	EJSR strcmp
+	POP 1,0
+
+	// strcmp result in AC2
+	// 0 = equal
+	MOV 2,2,SZR
+	JMP cmdtbl_next
+
+	// ========= MATCH FOUND =========
+
+	// ---- Clone frame template ----
+	LDA 2, -6, 3		// AC2 = template pointer
+	LDA 1, -5, 3		// AC3 = frame size (counter)
+
+cmdtbl_frame_loop:
+	MOV 1, 1, SNR		// if count == 0 → done
+	JMP cmdtbl_call
+
+	LDA 0, 0, 2		// AC0 = *template
+	PSH 0,0			// push word
+
+	INC 2, 2		// template++
+
+	// decrement AC3 (count--)
+	ELEF 0, 1
+	SUB 0, 1			// AC3 = AC3 - AC0
+
+	JMP cmdtbl_frame_loop
+
+cmdtbl_call:
+	// Load routine pointer: *(current + 1)
+	LDA 2, 1, 3		// AC2 = current
+	LDA 2, 1, 2		// AC2 = routine pointer
+
+	JSR 0, 2		// indirect call via AC2
+
+	LDA 1, -5, 3
+
+cmdtbl_popparams:
+	MOV 1, 1, SNR
+	JMP cmdtbl_done		// safety return
+
+	POP 0, 0
+
+	ELEF 0, 1
+	SUB 0, 1
+
+	JMP cmdtbl_popparams
+
+cmdtbl_next:
+	// current += 2
+	LDA 0, 1, 3
+	INC 0,0
+	INC 0,0
+	STA 0, 1, 3
+	JMP cmdtbl_loop
+
+cmdtbl_done:
+	RTN
 
 	// strtok - Tokenise a string
 	//
